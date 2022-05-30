5c41fdb90cfd0f84a2d8415ae4a144126281aeab
KUNKUN2121 <kunkun7532@gmail.com>
2022-05-30 21:20:48 +0900

    add http

--- lib/add/main.dart
+++ lib/add/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:http/http.dart' as http;
 
 class Add extends StatelessWidget {
   @override
