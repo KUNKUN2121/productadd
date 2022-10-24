38724b39025540b8c0cc81c9ba47059b6fa59765
KUNKUN2121 <kunkun7532@gmail.com>
2022-10-24 13:03:37 +0900

    ChangePostOK

--- lib/src/api/ChangePost.dart
+++ lib/src/api/ChangePost.dart
@@ -1,3 +1,5 @@
+// import 'dart:html';
+
 import 'package:http/http.dart' as http;
 import 'package:productadd/main.dart';
 import 'dart:convert';
@@ -7,18 +9,17 @@ import 'package:productadd/src/model/Barcode.dart';
 
 class Change {
   final int barcode;
-  final int quantity;
+  final String name;
   Change({
-    required this.quantity,
+    required this.name,
     required this.barcode,
   });
 
-  static Future<int> changePostQuantity(
-    int barnum,
-    int quantity,
-  ) async {
+  static Future<int> changePostQuantity(String barnum, String quantity) async {
     // 設定
     String url = apiURL + "changePostQuantity.php";
+    // String url = testURL + "changePostQuantity.php";
+    print(url);
     try {
       var resp = await http.post(Uri.parse(url), body: {
         'barnum': barnum,
@@ -29,32 +30,6 @@ class Change {
         print('RegisterPost Error Code : ${resp.statusCode}');
         return resp.statusCode;
       }
-      // print(resp.body);
-      print('レスポンスOK');
-      return 200;
-    } catch (e) {
-      print(e);
-      return 500;
-    }
-  }
-
-  static Future<int> changePostMore(
-    int barnum,
-    int quantity,
-  ) async {
-    // 設定
-    String url = apiURL + "changePost.php";
-    try {
-      var resp = await http.post(Uri.parse(url), body: {
-        'barnum': barnum,
-        'quantity': quantity,
-      });
-
-      if (resp.statusCode != 200) {
-        print('RegisterPost Error Code : ${resp.statusCode}');
-        return resp.statusCode;
-      }
-      // print(resp.body);
       print('レスポンスOK');
       return 200;
     } catch (e) {
